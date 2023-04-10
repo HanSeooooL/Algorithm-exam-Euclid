99608222e248811b3b31c128183c1734f203d116
HANSEOL <nothinonthere@icoud.com>
2023-04-10 16:51:09 +0900

    [UPDATE] + header

---
+++ 230406 exam Euclid/euclid.h
@@ -0,0 +1,17 @@
+//
+//  euclid.h
+//  230406 exam Euclid
+//
+//  Created by 한설 on 2023/04/06.
+//
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int euclidjaegui(int a, int b);
+int euclidloop(int a, int b);
+void TC_euclid(void);
+void TCJ(int *a, int *b, int *res);
+void TCl(int *a, int *b, int *res);
